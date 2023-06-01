59a2eee32911b774dcedce9f3c17395a09f2c183
root <root@DARLINTON-PC.localdomain>
2023-05-31 22:13:06 -0300

    Listando dados na view supports

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,26 @@
-<h1>Listagem dos suportes</h1>
+<h1>Listagem dos Suportes</h1>
+
+<table>
+    <thead>
+        <th>assuntos</th>
+        <th>status</th>
+        <th>descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @forelse ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @empty
+        <tr>
+            <td colspan="4">Nenhum registro encontrado.</td>
+        </tr>
+        @endforelse
+    </tbody>
+</table>
