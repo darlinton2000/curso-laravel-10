fbf29fcbd44db5c6300656861168b98d39a6fc95
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-06-28 20:49:02 -0300

    Tailwindcss implementado

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,33 +1,43 @@
-<h1>Listagem dos Suportes</h1>
+@extends('admin.layouts.app')
 
-<a href="{{ route('supports.create') }}">Criar Dúvida</a>
+@section('title', 'Fórum')
 
-<table>
-    <thead>
-        <th>assuntos</th>
-        <th>status</th>
-        <th>descrição</th>
-        <th></th>
-    </thead>
-    <tbody>
-        @forelse ($supports->items() as $support)
+@section('header')
+    <h1>Listagem dos Suportes</h1>
+@endsection
+
+@section('content') 
+
+    <a href="{{ route('supports.create') }}">Criar Dúvida</a>
+
+    <table>
+        <thead>
+            <th>assuntos</th>
+            <th>status</th>
+            <th>descrição</th>
+            <th></th>
+        </thead>
+        <tbody>
+            @forelse ($supports->items() as $support)
+                <tr>
+                    <td>{{ $support->subject }}</td>
+                    <td>{{ getStatusSupport($support->status) }}</td>
+                    <td>{{ $support->body }}</td>
+                    <td>
+                        <a href="{{ route('supports.show', $support->id) }}">Ir</a>
+                        <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
+                    </td>
+                </tr>
+            @empty
             <tr>
-                <td>{{ $support->subject }}</td>
-                <td>{{ getStatusSupport($support->status) }}</td>
-                <td>{{ $support->body }}</td>
-                <td>
-                    <a href="{{ route('supports.show', $support->id) }}">Ir</a>
-                    <a href="{{ route('supports.edit', $support->id) }}">Editar</a>
-                </td>
+                <td colspan="4">Nenhum registro encontrado.</td>
             </tr>
-        @empty
-        <tr>
-            <td colspan="4">Nenhum registro encontrado.</td>
-        </tr>
-        @endforelse
-    </tbody>
-</table>
+            @endforelse
+        </tbody>
+    </table>
 
-<x-pagination 
-    :paginator="$supports"
-    :appends="$filters" />
+    <x-pagination 
+        :paginator="$supports"
+        :appends="$filters" />
+
+@endsection
