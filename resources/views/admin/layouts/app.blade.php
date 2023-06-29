d0c4d41c501d72b909ca9a00582bdd16e839e41f
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-06-28 21:47:17 -0300

    Ajustando as views com o Tailwindcss

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,18 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }}</title>
-    
+
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    <head>
+    <section class="container px-4 mx-auto"> 
+
         @yield('header')
-    </head>
-    <div class="content">
-        @yield('content')
-    </div>
-    <footer>
-        #default footer
-    </footer>
+
+        <div>
+            @yield('content')
+        </div>
+        
+    </section>
 </body>
 </html>
